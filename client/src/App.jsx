6cfe8f879f98d359b6575c8665e63815e8f6ba67
Abhay Mishra <abhaymishra0902@gmail.com>
2023-10-26 14:50:01 +0530

    final_changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = '4xt8mvs7688h';
+const apiKey = 'w86fp357x668';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
